5aa3ad776c5b33a1cda1198060e64e7d7741efde
vatbub <vatbub123+github@googlemail.com>
2019-05-02 11:25:36 +0200

    [2] Refactored the code to improve code quality

--- app/src/main/java/com/github/vatbub/scoreboard/util/ArrayExtensions.kt
+++ app/src/main/java/com/github/vatbub/scoreboard/util/ArrayExtensions.kt
@@ -3,5 +3,5 @@ package com.github.vatbub.scoreboard.util
 inline fun <TIn, reified TOut> Array<out TIn>.transform(action: (TIn) -> TOut): Array<out TOut> =
         Array(this.size) { action(this[it]) }
 
-inline fun <TIn, reified TOut> List<out TIn>.transform(action: (TIn) -> TOut): List<out TOut> =
+inline fun <TIn, reified TOut> List<TIn>.transform(action: (TIn) -> TOut): List<TOut> =
         List(this.size) { action(this[it]) }
