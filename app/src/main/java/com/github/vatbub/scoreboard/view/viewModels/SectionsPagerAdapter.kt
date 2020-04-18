d22ab36bc07e98a4fe59236b861f356258372d9a
Frederik Kammel <vatbub123+github@googlemail.com>
2020-04-18 11:38:25 +0200

    Added german translations for the tab titles of the sync activity

--- app/src/main/java/com/github/vatbub/scoreboard/view/viewModels/SectionsPagerAdapter.kt
+++ app/src/main/java/com/github/vatbub/scoreboard/view/viewModels/SectionsPagerAdapter.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.github.vatbub.scoreboard.R
-import java.lang.IllegalArgumentException
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+        R.string.tab_host_title,
+        R.string.tab_join_title
 )
 
 /**
